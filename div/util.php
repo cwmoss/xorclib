34c1b6f3f5b7c643b5435fa88e472eccf368e7d9
Robert Wagner <rw@20sec.net>
2024-01-10 15:30:34 +0100

    cleanup

--- div/util.php
+++ div/util.php
@@ -6,234 +6,145 @@
 
 global $cp1252_map;
 $cp1252_map = array(
-   "\xc2\x80" => "\xe2\x82\xac", /* EURO SIGN */
-   "\xc2\x82" => "\xe2\x80\x9a", /* SINGLE LOW-9 QUOTATION MARK */
-   "\xc2\x83" => "\xc6\x92",    /* LATIN SMALL LETTER F WITH HOOK */
-   "\xc2\x84" => "\xe2\x80\x9e", /* DOUBLE LOW-9 QUOTATION MARK */
-   "\xc2\x85" => "\xe2\x80\xa6", /* HORIZONTAL ELLIPSIS */
-   "\xc2\x86" => "\xe2\x80\xa0", /* DAGGER */
-   "\xc2\x87" => "\xe2\x80\xa1", /* DOUBLE DAGGER */
-   "\xc2\x88" => "\xcb\x86",    /* MODIFIER LETTER CIRCUMFLEX ACCENT */
-   "\xc2\x89" => "\xe2\x80\xb0", /* PER MILLE SIGN */
-   "\xc2\x8a" => "\xc5\xa0",    /* LATIN CAPITAL LETTER S WITH CARON */
-   "\xc2\x8b" => "\xe2\x80\xb9", /* SINGLE LEFT-POINTING ANGLE QUOTATION */
-   "\xc2\x8c" => "\xc5\x92",    /* LATIN CAPITAL LIGATURE OE */
-   "\xc2\x8e" => "\xc5\xbd",    /* LATIN CAPITAL LETTER Z WITH CARON */
-   "\xc2\x91" => "\xe2\x80\x98", /* LEFT SINGLE QUOTATION MARK */
-   "\xc2\x92" => "\xe2\x80\x99", /* RIGHT SINGLE QUOTATION MARK */
-   "\xc2\x93" => "\xe2\x80\x9c", /* LEFT DOUBLE QUOTATION MARK */
-   "\xc2\x94" => "\xe2\x80\x9d", /* RIGHT DOUBLE QUOTATION MARK */
-   "\xc2\x95" => "\xe2\x80\xa2", /* BULLET */
-   "\xc2\x96" => "\xe2\x80\x93", /* EN DASH */
-   "\xc2\x97" => "\xe2\x80\x94", /* EM DASH */
+	"\xc2\x80" => "\xe2\x82\xac", /* EURO SIGN */
+	"\xc2\x82" => "\xe2\x80\x9a", /* SINGLE LOW-9 QUOTATION MARK */
+	"\xc2\x83" => "\xc6\x92",    /* LATIN SMALL LETTER F WITH HOOK */
+	"\xc2\x84" => "\xe2\x80\x9e", /* DOUBLE LOW-9 QUOTATION MARK */
+	"\xc2\x85" => "\xe2\x80\xa6", /* HORIZONTAL ELLIPSIS */
+	"\xc2\x86" => "\xe2\x80\xa0", /* DAGGER */
+	"\xc2\x87" => "\xe2\x80\xa1", /* DOUBLE DAGGER */
+	"\xc2\x88" => "\xcb\x86",    /* MODIFIER LETTER CIRCUMFLEX ACCENT */
+	"\xc2\x89" => "\xe2\x80\xb0", /* PER MILLE SIGN */
+	"\xc2\x8a" => "\xc5\xa0",    /* LATIN CAPITAL LETTER S WITH CARON */
+	"\xc2\x8b" => "\xe2\x80\xb9", /* SINGLE LEFT-POINTING ANGLE QUOTATION */
+	"\xc2\x8c" => "\xc5\x92",    /* LATIN CAPITAL LIGATURE OE */
+	"\xc2\x8e" => "\xc5\xbd",    /* LATIN CAPITAL LETTER Z WITH CARON */
+	"\xc2\x91" => "\xe2\x80\x98", /* LEFT SINGLE QUOTATION MARK */
+	"\xc2\x92" => "\xe2\x80\x99", /* RIGHT SINGLE QUOTATION MARK */
+	"\xc2\x93" => "\xe2\x80\x9c", /* LEFT DOUBLE QUOTATION MARK */
+	"\xc2\x94" => "\xe2\x80\x9d", /* RIGHT DOUBLE QUOTATION MARK */
+	"\xc2\x95" => "\xe2\x80\xa2", /* BULLET */
+	"\xc2\x96" => "\xe2\x80\x93", /* EN DASH */
+	"\xc2\x97" => "\xe2\x80\x94", /* EM DASH */
 
-   "\xc2\x98" => "\xcb\x9c",    /* SMALL TILDE */
-   "\xc2\x99" => "\xe2\x84\xa2", /* TRADE MARK SIGN */
-   "\xc2\x9a" => "\xc5\xa1",    /* LATIN SMALL LETTER S WITH CARON */
-   "\xc2\x9b" => "\xe2\x80\xba", /* SINGLE RIGHT-POINTING ANGLE QUOTATION*/
-   "\xc2\x9c" => "\xc5\x93",    /* LATIN SMALL LIGATURE OE */
-   "\xc2\x9e" => "\xc5\xbe",    /* LATIN SMALL LETTER Z WITH CARON */
-   "\xc2\x9f" => "\xc5\xb8"      /* LATIN CAPITAL LETTER Y WITH DIAERESIS*/
+	"\xc2\x98" => "\xcb\x9c",    /* SMALL TILDE */
+	"\xc2\x99" => "\xe2\x84\xa2", /* TRADE MARK SIGN */
+	"\xc2\x9a" => "\xc5\xa1",    /* LATIN SMALL LETTER S WITH CARON */
+	"\xc2\x9b" => "\xe2\x80\xba", /* SINGLE RIGHT-POINTING ANGLE QUOTATION*/
+	"\xc2\x9c" => "\xc5\x93",    /* LATIN SMALL LIGATURE OE */
+	"\xc2\x9e" => "\xc5\xbe",    /* LATIN SMALL LETTER Z WITH CARON */
+	"\xc2\x9f" => "\xc5\xb8"      /* LATIN CAPITAL LETTER Y WITH DIAERESIS*/
 );
 
-function isowintoutf8($t){
+function isowintoutf8($t) {
 	global $cp1252_map;
-	$t = utf8_encode($t);
+	$t = mb_convert_encoding($t, 'UTF-8', 'Windows-1252');
 	return  strtr($t, $cp1252_map);
 }
 
-function utf8toisowin($t){
+function utf8toisowin($t) {
 	global $cp1252_map;
 	$t = strtr($t, array_flip($cp1252_map));
 
-	$t = utf8_decode($t);
-    return $t;
+	$t = mb_convert_encoding($t, 'Windows-1252', 'UTF-8');
+	return $t;
 }
 
-$_DEBUGLEVEL=0;
-function dump($var='', $msg='', $level=''){
-	global $_DEBUGLEVEL;
-	if($_DEBUGLEVEL){
-		if(!$var){
-			print("<br>DUMPMSG: <b>$msg</b><br>");
-		}else{
-			print("<br>DUMPVAR: <b>$msg</b> <pre>"); print_r($var); print("</pre><br>");
+function xorc_fetch_url($url, $proxy = null, $file = null, $verbose = false, &$hdrs = null) {
+	if ($proxy) {
+		$pp = parse_url($proxy);
+		$host = $pp['host'];
+		$port = $pp['port'];
+		if ($pp['user']) {
+			$auth = "Proxy-Authorization: Basic " . base64_encode($pp['user'] . ":" . $pp['pass']) . "\r\n";
 		}
-	}
-}
-
-function mQuote($str){
-	//return str_replace("'", "\\'", $str);
-	return mysql_escape_string($str);
-}
-/*
-	make urlencoded http get string from hash array
-*/
-function GET($vars, $url=""){
-	$ret = trim($url);
-	foreach($vars as $k=>$v){
-		$ret.=($c)?"?":"&";
-		$ret.=urlencode($k)."=".urlencode($v);
-		$c++;
-	}	
-	return $ret;
-}
-function getNumArchPath($id){
-	return sprintf("/%03d/%03d/%s", ($id/100000), $id/100, $id);
-}
-
-/*
-returns PATH for different objects $prop musthave
-*/
-function getFileNames($prop, $i = ""){
-		global $_dk, $_site;
-		$ret = array();
-		if(!$prop["id"]) return $ret;
-		$path = sprintf("%s*", getNumArchPath($prop["id"]));
-		echo "local::".$_site->getLocal($prop["prodmodul"]);
-		$ret["version"] = $prop["version"];
-		if(!isset($ret["version"]))$ret["version"] = 1;
-		if($i) $i2="_$i";
-		$ret["filename$i"] = sprintf("%s%s%sv%s%s",
-			$_site->getLocal($prop["prodmodul"]),
-			getNumArchPath($prop["id"]),
-			$i2, $prop["version"],
-			$prop["filetype"]);
-
-		$ret["fileurl$i"] = sprintf("%s%s%sv%s%s",
-			$_site->getUrl($prop["prodmodul"]),
-			getNumArchPath($prop["id"]),
-			$i2, $ret["version"], $prop["filetype"]);
-		return $ret;
-	}
-
-function imtab($arr){ 
-
-		$t = "<table align=\"left\" hspace=5 vspace=5>";
-		for($i=0; $i<sizeof($arr); $i=$i+4){
-		$u = $i;
-		if(sizeof($arr)-$i == 4){
-			$t .= "<tr><td><img src='/alotta/user/platoondvd/img/{$arr[$u]['picpath']}t3{$arr[$u]['id']}.jpg' border=0></td>";
-			$t .= "<td><img src='/alotta/user/platoondvd/img/{$arr[$u++]['picpath']}t3{$arr[$u]['id']}.jpg' border=0></td></tr>";
-			$t .= "<tr><td><img src='/alotta/user/platoondvd/img/{$arr[$u++]['picpath']}t3{$arr[$u]['id']}.jpg' border=0></td>";
-			$t .= "<td><img src='/alotta/user/platoondvd/img/{$arr[$u++]['picpath']}t3{$arr[$u]['id']}.jpg' border=0></td></tr> ";
-			break;
-		}elseif(sizeof($arr)-$i == 3){
-			$t .= "<tr><td><img src='/alotta/user/platoondvd/img/{$arr[$u]['picpath']}t3{$arr[$u]['id']}.jpg' border=0></td>";
-			$t .= "<td><img src='/alotta/user/platoondvd/img/{$arr[$u++]['picpath']}t3{$arr[$u]['id']}.jpg' border=0></td></tr>";
-			$t .= "<tr><td colspan=2><img src='/alotta/user/platoondvd/img/{$arr[$u++]['picpath']}t2{$arr[$u]['id']}.jpg' border=0></td></tr>";
-			break;
-		}elseif(sizeof($arr)-$i == 2){
-			$t .= "<tr><td><img src='/alotta/user/platoondvd/img/{$arr[$u]['picpath']}t3{$arr[$u]['id']}.jpg' border=0></td>";
-			$t .= "<td><img src='/alotta/user/platoondvd/img/{$arr[$u++]['picpath']}t3{$arr[$u]['id']}.jpg' border=0></td></tr>";
-			break;
-		}elseif(sizeof($arr)-$i == 1){
-			$t .= "<tr><td colspan=2><img src='/alotta/user/platoondvd/img/{$arr[$u]['picpath']}t2{$arr[$i]['id']}.jpg' border=0></></tr>";
-			break;
-		}else{
-			$t .= "<tr><td><img src='/alotta/user/platoondvd/img/{$arr[$u]['picpath']}t3{$arr[$u]['id']}.jpg' border=0></td>";
-			$t .= "<td><img src='/alotta/user/platoondvd/img/{$arr[$u++]['picpath']}t3{$arr[$u]['id']}.jpg' border=0></td></tr>";
-			$t .= "<tr><td><img src='/alotta/user/platoondvd/img/{$arr[$u++]['picpath']}t3{$arr[$u]['id']}.jpg' border=0></td> ";
-			$t .= "<td><img src='/alotta/user/platoondvd/img/{$arr[$u++]['picpath']}t3{$arr[$u]['id']}.jpg' border=0></td></tr>";
+		$doc = $url;
+	} else {
+		$pp = parse_url($url);
+		$host = $pp['host'];
+		$port = $pp['port'];
+		if (!$port) {
+			if ($pp['scheme'] == 'https') $port = "443";
+			else $port = "80";
 		}
-		}
-		$t .= "</table>";
-return $t;
-
-
-}
-
-function xorc_fetch_url($url, $proxy=null, $file=null, $verbose=false, &$hdrs=null){
-	if($proxy){
-		$pp=parse_url($proxy);
-		$host=$pp['host'];
-		$port=$pp['port'];
-		if($pp['user']){
-			$auth="Proxy-Authorization: Basic " . base64_encode ($pp['user'].":".$pp['pass']) . "\r\n";
-		}
-		$doc=$url;
-	}else{
-		$pp=parse_url($url);
-		$host=$pp['host'];
-		$port=$pp['port'];
-		if(!$port){
-		   if($pp['scheme']=='https') $port = "443";
-		   else $port="80";
-		}
-		$doc=$pp['path'];
-		if($pp['query']) $doc.="?".$pp['query'];
+		$doc = $pp['path'];
+		if ($pp['query']) $doc .= "?" . $pp['query'];
 		# Authorization: BASIC SFRNTFdvcmxkOkludGVybmV0
-		if($pp['user']){
-			$auth="Authorization: Basic " . base64_encode ($pp['user'].":".$pp['pass']) . "\r\n";
+		if ($pp['user']) {
+			$auth = "Authorization: Basic " . base64_encode($pp['user'] . ":" . $pp['pass']) . "\r\n";
 		}
 	}
 	$shost = $host;
 	$scheme = $pp['scheme'];
-	if($scheme && $scheme != 'http'){
-	   if($scheme == 'https') $scheme = 'ssl';
-	   $shost = $scheme.'://'.$shost;
+	if ($scheme && $scheme != 'http') {
+		if ($scheme == 'https') $scheme = 'ssl';
+		$shost = $scheme . '://' . $shost;
 	}
 	$fp = fsockopen($shost, $port, $errno, $errstr, 30);
-	if(!$fp) return false;
-	if($file) $out=fopen($file, "w");
-	if($verbose) print "GET $doc HTTP/1.0\r\nHost: $host\r\nevtl. write to file $file\n\n";
-   fputs($fp, "GET $doc HTTP/1.0\r\nHost: $host\r\n");
-   if($auth) fputs($fp, $auth);
+	if (!$fp) return false;
+	if ($file) $out = fopen($file, "w");
+	if ($verbose) print "GET $doc HTTP/1.0\r\nHost: $host\r\nevtl. write to file $file\n\n";
+	fputs($fp, "GET $doc HTTP/1.0\r\nHost: $host\r\n");
+	if ($auth) fputs($fp, $auth);
 	fputs($fp, "\r\n\r\n");
-	$header=false;$data=$headerdata="";
-   while(!feof($fp)){
+	$header = false;
+	$data = $headerdata = "";
+	while (!feof($fp)) {
 		$buffer = fread($fp, 4096);
-		if(!$header && (strpos($buffer, "\r\n\r\n")!==false)){
-			$headerdata.=substr($buffer, 0, strpos($buffer, "\r\n\r\n"));
-			$buffer=substr($buffer, strpos($buffer, "\r\n\r\n")+4);
-			$header=true;
+		if (!$header && (strpos($buffer, "\r\n\r\n") !== false)) {
+			$headerdata .= substr($buffer, 0, strpos($buffer, "\r\n\r\n"));
+			$buffer = substr($buffer, strpos($buffer, "\r\n\r\n") + 4);
+			$header = true;
 		}
-		if($header){
-			if($out) fwrite($out, $buffer);
-			else $data.=$buffer;	
+		if ($header) {
+			if ($out) fwrite($out, $buffer);
+			else $data .= $buffer;
 		}
 	}
-   fclose($fp);
-	if($out) fclose($out);
-	if(!(null===$hdrs)) $hdrs=xorc_parse_httpheaders($headerdata);
-   return $data;
+	fclose($fp);
+	if ($out) fclose($out);
+	if (!(null === $hdrs)) $hdrs = xorc_parse_httpheaders($headerdata);
+	return $data;
 }
 
 /* 
  * modified from php.net comment by luigi dot sexpistols at gmail dot com
  *    http://de.php.net/manual/de/function.http-parse-headers.php
 */
-function xorc_parse_httpheaders($headers=false){
-   if($headers === false) return false;
-   $headers = str_replace("\r","",$headers);
-   $headers = explode("\n",$headers);
-   foreach($headers as $value){
-      $header = explode(": ",$value);
-      if($header[0] && !$header[1]){
-         $status=preg_split("/\s+/", $header[0]);
-         $headerdata['status'] = $status[1];
-         $headerdata['proto'] = $status[0];
-         $headerdata['msg'] = $status[2];
-      }elseif($header[0] && $header[1]){
-         $headerdata[strtolower($header[0])] = $header[1];
-      }
-   }
-   return $headerdata;
+function xorc_parse_httpheaders($headers = false) {
+	if ($headers === false) return false;
+	$headers = str_replace("\r", "", $headers);
+	$headers = explode("\n", $headers);
+	foreach ($headers as $value) {
+		$header = explode(": ", $value);
+		if ($header[0] && !$header[1]) {
+			$status = preg_split("/\s+/", $header[0]);
+			$headerdata['status'] = $status[1];
+			$headerdata['proto'] = $status[0];
+			$headerdata['msg'] = $status[2];
+		} elseif ($header[0] && $header[1]) {
+			$headerdata[strtolower($header[0])] = $header[1];
+		}
+	}
+	return $headerdata;
 }
 
 function addLinks($string) {
-  $string = preg_replace(
-       "/(?<!quot;|[=\"]|:\/\/)\b((\w+:\/\/|www\.).+?)".
-       "(?=\W*([<>\s]|$))/i", "<a href=\"$1\">$1</a>", $string);
-  return preg_replace("/href=\"www/i", "href=\"http://www", $string);#}
+	$string = preg_replace(
+		"/(?<!quot;|[=\"]|:\/\/)\b((\w+:\/\/|www\.).+?)" .
+			"(?=\W*([<>\s]|$))/i",
+		"<a href=\"$1\">$1</a>",
+		$string
+	);
+	return preg_replace("/href=\"www/i", "href=\"http://www", $string); #}
 
 	//$t = addLinks($t);
-return $string;}
+	return $string;
+}
 
 # wordpress
-if(!function_exists('is_email')){
-function is_email($email){
-return(preg_match("/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|in|info|int|io|iq|ir|is|it|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)|(([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5]))$/i",$email));
+if (!function_exists('is_email')) {
+	function is_email($email) {
+		return (preg_match("/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|in|info|int|io|iq|ir|is|it|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)|(([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5]))$/i", $email));
+	}
 }
-}
-?>
